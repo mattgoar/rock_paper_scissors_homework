c8f286944df606282021aac769cac3db6ddd0e90
mattgoar <mattgoar@gmail.com>
2015-07-27 14:09:24 -0500

    Part 1 Complete and built database

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -6,4 +6,39 @@ class GameController < ApplicationController
 
   # Your code goes below.
 
+
+  def rps
+    # Nothing to do here.
+    @computer_move = computer_move
+    @your_move = params["the_move"]
+    @result = who_wins(@your_move, @computer_move)
+    puts @result
+    render("move.html.erb")
+  end
+
+  def computer_move
+    moves = ['Rock','Paper','Scissors']
+    return moves[rand(3)]
+  end
+
+  def who_wins (human, computer_move)
+    winners =
+    [['rock', 'scissors'],
+    ['scissors', 'paper'],
+    ['paper', 'rock']]
+    if winners.include?(Array[human.downcase, computer_move.downcase])
+      return "win"
+    elsif human.downcase == computer_move.downcase
+      return "tied"
+    else
+      return "lose"
+    end
+  end
+end
+
+
+
+class RpsController < ApplicationController
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # This route is for the bare domain. You can ignore it.
   get("/", { :controller => "game", :action => "home" })
-
+  get("/play/:the_move", { :controller => "game", :action => "rps" })
   # Your code goes below.
 
 end
